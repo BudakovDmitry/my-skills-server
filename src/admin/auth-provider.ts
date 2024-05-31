e098c1db740c2c5d68f1418517f7aa56ec9a7f98
dmitriy.budakov <dmitry.budakov@forforce.com>
2024-05-31 23:26:49 +0300

    Update provider

--- src/admin/auth-provider.ts
+++ src/admin/auth-provider.ts
@@ -3,9 +3,6 @@ import { DefaultAuthProvider } from 'adminjs';
 import componentLoader from './component-loader.js';
 import { DEFAULT_ADMIN } from './constants.js';
 
-/**
- * Make sure to modify "authenticate" to be a proper authentication method
- */
 const provider = new DefaultAuthProvider({
   componentLoader,
   authenticate: async ({ email, password }) => {
